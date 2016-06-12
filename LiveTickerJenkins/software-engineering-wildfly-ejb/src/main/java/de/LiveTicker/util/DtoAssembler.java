6712a281c01173e4a75be990ce39620a97f7d1fd
Dominik Teipel <teipel-dominik@web.de>
2016-06-12 19:50:07 +0200

    klasse game

--- LiveTickerJenkins/software-engineering-wildfly-ejb/src/main/java/de/LiveTicker/util/DtoAssembler.java
+++ LiveTickerJenkins/software-engineering-wildfly-ejb/src/main/java/de/LiveTicker/util/DtoAssembler.java
@@ -1,8 +1,10 @@
 package de.LiveTicker.util;
 
+import javax.ejb.Stateless;
 import de.LiveTicker.dto.UserTO;
 import de.LiveTicker.entities.User;
 
+@Stateless
 public class DtoAssembler {
 
 	public UserTO makeDTO(User user) {
