b59513ed15fd5c35126d003d8c50d1f233dddbb4
Dominik Teipel <teipel-dominik@web.de>
2016-06-20 01:40:18 +0200

    javadoc und Logik

--- LiveTickerJenkins/software-engineering-wildfly-ejb/src/main/java/de/LiveTicker/entities/Event.java
+++ LiveTickerJenkins/software-engineering-wildfly-ejb/src/main/java/de/LiveTicker/entities/Event.java
@@ -18,104 +18,146 @@ public class Event implements Serializable {
 	/**
 	 * Attribute
 	 */
-	
+
 	@Id
 	private int id;
-	@Column(nullable=false)
-	private int art;
-	@Column(nullable=false)
+	@Column(nullable = false)
+	private String art;
+	@Column(nullable = false)
 	private int team;
-	@Column(nullable=true)
+	@Column(nullable = true)
 	private String reason;
-	@Column(nullable=false)
+	@Column(nullable = false)
 	private int min;
-	@Column(nullable=false)
+	@Column(nullable = false)
 	private int gameid;
+
 	/**
 	 * Leere Konstruktor
 	 */
-	public Event(){
-		
+	public Event() {
+
 	}
+
 	/**
-	 * Konstruktor
-	 * Ein event gehört immer zu einem Spiel
-	 * @param gameid 
+	 * Konstruktor Ein event gehört immer zu einem Spiel
+	 * 
+	 * @param gameid
 	 * @param art
 	 * @param team
 	 * @param reason
 	 * @param min
 	 */
-	
-	public Event (int gameid,int art, int team,String reason, int min){
-		this.art=art;
-		this.team=team;
-		this.reason=reason;
-		this.min=min;
-		this.gameid=gameid;
+
+	public Event(int gameid, int art, int team, String reason, int min) {
+		whatArt(art);
+		this.team = team;
+		this.reason = reason;
+		this.min = min;
+		this.gameid = gameid;
 	}
+
 	/**
 	 * Gibt die ID des Events züruck
+	 * 
 	 * @return id
 	 */
-	public int getId(){
+	public int getId() {
 		return id;
 	}
-	public int getArt(){
-		return art;
+
+	public int getArt() {
+		return whatInt(art);
 	}
-	public void setArt(int art){
-		this.art=art;
+
+	public void setArt(int art) {
+		whatArt(art);
 	}
-	public int getTeam(){
+
+	public int getTeam() {
 		return team;
 	}
-	public void setTeam(int team){
-		this.team=team;
+
+	public void setTeam(int team) {
+		this.team = team;
 	}
-	public String getReason(){
-		return reason;	
+
+	public String getReason() {
+		return reason;
 	}
-	public void setReason(String reason){
-		this.reason=reason;
+
+	public void setReason(String reason) {
+		this.reason = reason;
 	}
-	public int getMin(){
-		return min;	
+
+	public int getMin() {
+		return min;
 	}
-	public void setMin(int min){
-		this.min=min;
+
+	public void setMin(int min) {
+		this.min = min;
 	}
-	public int getGameID(){
+
+	public int getGameID() {
 		return gameid;
 	}
-	public void setGameID(int gameid){
-		this.gameid=gameid;
+
+	public void setGameID(int gameid) {
+		this.gameid = gameid;
 	}
-	/**
-	public void whatArt(int art){
-		
-		switch(art){
+
+	public void whatArt(int art) {
+
+		switch (art) {
 		// gelbe karte
-		case 1: reason="gelbe karte";
+		case 1:
+			this.reason = "gelbe karte";
 			break;
 		// rote karte
-		case 2: reason="rote karte";
+		case 2:
+			this.reason = "rote karte";
 			break;
 		// freistoß
-		case 3: reason="freistoß";
+		case 3:
+			this.reason = "freistoß";
 			break;
 		// spielerwechsel
-		case 4: reason="Tor";
+		case 4:
+			this.reason = "Tor";
 			break;
 		// tor
-		case 5: reason="Spielerwechsel";
+		case 5:
+			this.reason = "Spielerwechsel";
 			break;
-		//chance
-		case 6: reason="Chance";
+		// chance
+		case 6:
+			this.reason = "Chance";
+			break;
 		// abpfiff
-		case 7: reason="Spiel ist beendet.";
+		case 7:
+			this.reason = "Spiel ist beendet.";
 			break;
-		
+
 		}
-	}*/
+	}
+
+	public int whatInt(String art) {
+		switch (art) {
+		case "gelbe karte":
+			return 1;
+		case "rote karte":
+			return 2;
+		case "freistoß":
+			return 3;
+		case "Tor":
+			return 4;
+		case "Spielerwechsel":
+			return 5;
+		case "Chance":
+			return 6;
+		case "Spiel ist beendet.":
+			return 7;
+		}
+		return 0;
+	}
 }
